e4289885d9d46533ab46fa24d2020811da936302
rojlarge <rojlarge@iu.edu>
2015-09-01 18:24:32 -0400

    Removing duplicate local variables in ProtocolAttachmentType

--- coeus-impl/src/main/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentType.java
+++ coeus-impl/src/main/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentType.java
@@ -27,10 +27,6 @@ public class ProtocolAttachmentType extends ProtocolAttachmentTypeBase {
 
     private static final long serialVersionUID = 2053606476193782286L;
 
-    private String code;
-
-    private String description;
-
     /**
      * empty ctor to satisfy JavaBean convention.
      */
@@ -49,77 +45,6 @@ public class ProtocolAttachmentType extends ProtocolAttachmentTypeBase {
      * @param description the description.
      */
     public ProtocolAttachmentType(String code, String description) {
-        this.code = code;
-        this.description = description;
-    }
-
-    /**
-     * Gets the protocol attachment type code.
-     * @return the protocol attachment type code
-     */
-    public String getCode() {
-        return this.code;
-    }
-
-    /**
-     * Sets the protocol attachment type code.
-     * @param code the protocol attachment type code
-     */
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    /**
-     * Gets the protocol attachment type description.
-     * @return the protocol attachment type description
-     */
-    public String getDescription() {
-        return this.description;
-    }
-
-    /**
-     * Sets the protocol attachment type description.
-     * @param description the protocol attachment type description
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.code == null) ? 0 : this.code.hashCode());
-        result = prime * result + ((this.description == null) ? 0 : this.description.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof ProtocolAttachmentType)) {
-            return false;
-        }
-        ProtocolAttachmentType other = (ProtocolAttachmentType) obj;
-        if (this.code == null) {
-            if (other.code != null) {
-                return false;
-            }
-        } else if (!this.code.equals(other.code)) {
-            return false;
-        }
-        if (this.description == null) {
-            if (other.description != null) {
-                return false;
-            }
-        } else if (!this.description.equals(other.description)) {
-            return false;
-        }
-        return true;
+        super(code, description);
     }
 }
